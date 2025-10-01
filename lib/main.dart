0652cf41963abea68e86030861f36a560c218dee
Romantico <guylhermeeafonso@gmail.com>
2025-10-01 19:05:55 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,288 +1,112 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MeuApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Staggered Menu Demo',
+      title: 'Layout Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        scaffoldBackgroundColor: Colors.white,
-      ),
-      home: const ExampleStaggeredAnimations(),
-    );
-  }
-}
-
-class ExampleStaggeredAnimations extends StatefulWidget {
-  const ExampleStaggeredAnimations({super.key});
-  @override
-  State<ExampleStaggeredAnimations> createState() =>
-      _ExampleStaggeredAnimationsState();
-}
-
-class _ExampleStaggeredAnimationsState extends State<ExampleStaggeredAnimations>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _drawerSlideController;
-
-  @override
-  void initState() {
-    super.initState();
-    _drawerSlideController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 150),
-    );
-  }
-
-  @override
-  void dispose() {
-    _drawerSlideController.dispose();
-    super.dispose();
-  }
-
-  bool _isDrawerOpen() {
-    return _drawerSlideController.value == 1.0;
-  }
-
-  bool _isDrawerOpening() {
-    return _drawerSlideController.status == AnimationStatus.forward;
-  }
-
-  bool _isDrawerClosed() {
-    return _drawerSlideController.value == 0.0;
-  }
-
-  void _toggleDrawer() {
-    if (_isDrawerOpen() || _isDrawerOpening()) {
-      _drawerSlideController.reverse();
-    } else {
-      _drawerSlideController.forward();
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: _buildAppBar(),
-      body: Stack(children: [_buildContent(), _buildDrawer()]),
-    );
-  }
-
-  PreferredSizeWidget _buildAppBar() {
-    return AppBar(
-      title: const Text('Menu Animado', style: TextStyle(color: Colors.white)),
-      backgroundColor: Colors.deepPurple,
-      elevation: 0.0,
-      automaticallyImplyLeading: false,
-      actions: [
-        AnimatedBuilder(
-          animation: _drawerSlideController,
-          builder: (context, child) {
-            return IconButton(
-              onPressed: _toggleDrawer,
-              icon: _isDrawerOpen() || _isDrawerOpening()
-                  ? const Icon(Icons.clear, color: Colors.white)
-                  : const Icon(Icons.menu, color: Colors.white),
-            );
-          },
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Layout Demo'),
         ),
-      ],
-    );
-  }
+        body: Column(
+          children: [
+            // 1. A IMAGEM
+            Image.asset(
+              'images/lake.png',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover, 
+            ),
 
-  Widget _buildContent() {
-    // Conteúdo principal da página (vazio ou o que você quiser colocar)
-    return const Center(
-      child: Text(
-        'Página principal',
-        style: TextStyle(fontSize: 24),
+            _buildTitleSection(),
+
+            _buildButtonSection(),
+
+            _buildTextSection(),
+          ],
+        ),
       ),
     );
   }
 
-  Widget _buildDrawer() {
-    return AnimatedBuilder(
-      animation: _drawerSlideController,
-      builder: (context, child) {
-        return FractionalTranslation(
-          translation: Offset(1.0 - _drawerSlideController.value, 0.0),
-          child: _isDrawerClosed() ? const SizedBox() : const Menu(),
-        );
-      },
-    );
-  }
-}
 
-class Menu extends StatefulWidget {
-  const Menu({super.key});
-  @override
-  State<Menu> createState() => _MenuState();
-}
-
-class _MenuState extends State<Menu> with SingleTickerProviderStateMixin {
-  static const _menuTitles = [
-    'Criar Evento',
-    'Meus Eventos',
-    'Inscrições',
-    'Painel Organizador',
-    'Calendário',
-  ];
-  static const _initialDelayTime = Duration(milliseconds: 50);
-  static const _itemSlideTime = Duration(milliseconds: 250);
-  static const _staggerTime = Duration(milliseconds: 50);
-  static const _buttonDelayTime = Duration(milliseconds: 150);
-  static const _buttonTime = Duration(milliseconds: 500);
-
-  late AnimationController _staggeredController;
-  final List<Interval> _itemSlideIntervals = [];
-  late Interval _buttonInterval;
-
-  @override
-  void initState() {
-    super.initState();
-    _createAnimationIntervals();
-    _staggeredController = AnimationController(
-      vsync: this,
-      duration: _animationDuration,
-    )..forward();
-  }
-
-  void _createAnimationIntervals() {
-    for (var i = 0; i < _menuTitles.length; ++i) {
-      final startTime = _initialDelayTime + (_staggerTime * i);
-      final endTime = startTime + _itemSlideTime;
-      _itemSlideIntervals.add(
-        Interval(
-          startTime.inMilliseconds / _animationDuration.inMilliseconds,
-          endTime.inMilliseconds / _animationDuration.inMilliseconds,
-        ),
-      );
-    }
-    final buttonStartTime =
-        Duration(milliseconds: _menuTitles.length * 50) + _buttonDelayTime;
-    final buttonEndTime = buttonStartTime + _buttonTime;
-    _buttonInterval = Interval(
-      buttonStartTime.inMilliseconds / _animationDuration.inMilliseconds,
-      buttonEndTime.inMilliseconds / _animationDuration.inMilliseconds,
-    );
-  }
-
-  Duration get _animationDuration => _initialDelayTime +
-      (_staggerTime * _menuTitles.length) +
-      _buttonDelayTime +
-      _buttonTime;
-
-  @override
-  void dispose() {
-    _staggeredController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _buildTitleSection() {
     return Container(
-      color: Colors.white,
-      child: Stack(
-        fit: StackFit.expand,
-        children: [_buildLogo(), _buildContent()],
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start, 
+              children: [
+                Text(
+                  'TESTE',
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: Text(
+                    'TESTE',
+                    style: TextStyle(color: Colors.grey[500]),
+                  ),
+                ),
+              ],
+            ),
+          ),  
+          Icon(Icons.star, color: Colors.red[500]),
+          const Text('41'),
+        ],
       ),
     );
   }
 
-  Widget _buildLogo() {
-    return const Positioned(
-      right: -100,
-      bottom: -30,
-      child: Opacity(opacity: 0.2, child: FlutterLogo(size: 400)),
-    );
-  }
-
-  Widget _buildContent() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+  Widget _buildButtonSection() {
+    Color color = Colors.blue;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
       children: [
-        const SizedBox(height: 16),
-        ..._buildListItems(),
-        const Spacer(),
-        _buildGetStartedButton(),
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
       ],
     );
   }
 
-  List<Widget> _buildListItems() {
-    final listItems = <Widget>[];
-    for (var i = 0; i < _menuTitles.length; ++i) {
-      listItems.add(
-        AnimatedBuilder(
-          animation: _staggeredController,
-          builder: (context, child) {
-            final animationPercent = Curves.easeOut.transform(
-              _itemSlideIntervals[i].transform(_staggeredController.value),
-            );
-            final opacity = animationPercent;
-            final slideDistance = (1.0 - animationPercent) * 150;
-            return Opacity(
-              opacity: opacity,
-              child: Transform.translate(
-                offset: Offset(slideDistance, 0),
-                child: child,
-              ),
-            );
-          },
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 16),
-            child: Text(
-              _menuTitles[i],
-              style: const TextStyle(
-                  fontSize: 24, fontWeight: FontWeight.w500),
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
             ),
           ),
         ),
-      );
-    }
-    return listItems;
+      ],
+    );
   }
 
-  Widget _buildGetStartedButton() {
-    return SizedBox(
-      width: double.infinity,
-      child: Padding(
-        padding: const EdgeInsets.all(24),
-        child: AnimatedBuilder(
-          animation: _staggeredController,
-          builder: (context, child) {
-            final animationPercent = Curves.elasticOut.transform(
-              _buttonInterval.transform(_staggeredController.value),
-            );
-            final opacity = animationPercent.clamp(0.0, 1.0);
-            final scale = (animationPercent * 0.5) + 0.5;
-            return Opacity(
-              opacity: opacity,
-              child: Transform.scale(scale: scale, child: child),
-            );
-          },
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              shape: const StadiumBorder(),
-              backgroundColor: Colors.deepPurple,
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 48, vertical: 14),
-            ),
-            onPressed: () {
-              // ação do botão
-            },
-            child: const Text(
-              'Get Started',
-              style: TextStyle(color: Colors.white, fontSize: 22),
-            ),
-          ),
-        ),
+  Widget _buildTextSection() {
+    return const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'TESTE',
+        softWrap: true, 
       ),
     );
   }
